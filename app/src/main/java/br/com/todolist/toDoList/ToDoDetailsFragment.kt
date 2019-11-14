8ff3a6ab6d1b1d084729e8142b8e2e51b0d916d8
Fernanda Braga <fernandafbc@outlook.com>
2019-11-14 10:19:29 -0300

    Editar o nome e a descrição

--- app/src/main/java/br/com/todolist/toDoList/ToDoDetailsFragment.kt
+++ app/src/main/java/br/com/todolist/toDoList/ToDoDetailsFragment.kt
@@ -6,17 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 
 import br.com.todolist.R
+import br.com.todolist.ToDoListApplication
 import br.com.todolist.toDoList.model.ToDo
+import kotlinx.android.synthetic.main.fragment_to_do_details.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class ToDoDetailsFragment : Fragment() {
 
-    private var itemId: String? = null
+    private var toDo: ToDo? = null
+    private lateinit var itemId: String
+    var isEditing = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,20 +37,55 @@ class ToDoDetailsFragment : Fragment() {
         // o getArguments(arguments no kotlin) pega os argumentos que foram passados
         // e eu coloco na variavel itemId
         // fazendo isso vc precisa pegar esse id e pegar ele na database
-        itemId = arguments?.getString("itemId")
+        itemId = arguments?.getString("itemId") ?: ""
 
         retrieveItemDb()
+
+        deleteBtn.setOnClickListener {
+
+        }
+
+        editBtn.setOnClickListener {
+
+            if(isEditing)
+            {
+                toDo?.let { toDoItem ->
+                    val newToDo = ToDo(
+                        toDoItem.id,
+                        nameEdT.text.toString(),
+                        descriptionEdT.text.toString(),
+                        toDoItem.check
+                    )
+
+                    ToDoListApplication.database?.toDo()?.insertToDo(newToDo)
+                }
+                it.findNavController().popBackStack()
+
+            }
+            else{
+                nameEdT.isEnabled = true
+                descriptionEdT.isEnabled = true
+                editBtn.text = "concluir"
+                isEditing = true
+            }
+        }
     }
 
     private fun retrieveItemDb() {
         // pegar o a tarefa
 
+        toDo = ToDoListApplication.database?.toDo()?.loadSingle(itemId)
+
         // mostrar na tela os dados dela
-//        showToDoInformation(toDoItem)
+        showToDoInformation(toDo)
     }
 
-    private fun showToDoInformation(toDoItem: ToDo) {
+    private fun showToDoInformation(toDoItem: ToDo?) {
         // mostrar
+        toDoItem?.let {
+            nameEdT.setText(it.name)
+            descriptionEdT.setText(it.description)
+        }
     }
 
 }
--- app/src/main/java/br/com/todolist/toDoList/database/ToDoDao.kt
+++ app/src/main/java/br/com/todolist/toDoList/database/ToDoDao.kt
@@ -1,19 +1,22 @@
 package br.com.todolist.toDoList.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import br.com.todolist.toDoList.model.ToDo
 
 @Dao
-interface ToDoDao{
+interface ToDoDao {
 
     @Query("SELECT * FROM toDo")
-    fun getAll():List<ToDo>
+    fun getAll(): List<ToDo>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertToDo(toDo: ToDo)
+
+    @Query("DELETE FROM todo WHERE id = :id")
+    fun delete(id: String)
+
+    @Query("SELECT * FROM toDo WHERE id=:id ")
+    fun loadSingle(id: String): ToDo
 
     // criar o pegar um unico item
     // procurar na internet como
